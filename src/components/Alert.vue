4838138cd66071918635712675881604dd5f11f6
任明磊 <minglei.ren@wenba100.com>
2016-10-04 14:33:16 +0800

    alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="vue-alert">
-    <div class="alert-text alert alert-warning">{{ msg }}</div>
+  <div class="vue-alert" v-show="!!message.message">
+    <div class="alert-text alert alert-warning">{{ message.message }}</div>
   </div>
 </template>
 
 <script>
+import {message} from 'src/vuex/getters'
+
 export default {
+  vuex: {
+    getters: {
+      message
+    }
+  },
   data () {
     return {
       msg: 'Hello World!'
