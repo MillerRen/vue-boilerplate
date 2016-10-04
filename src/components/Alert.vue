003be464c69b7984233a7d8f51db9a3d453c91ca
任明磊 <minglei.ren@wenba100.com>
2016-10-04 14:13:57 +0800

    alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="vue-alert">
+    <div class="alert-text alert alert-warning">{{ msg }}</div>
   </div>
 </template>
 
@@ -8,19 +8,21 @@
 export default {
   data () {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
       msg: 'Hello World!'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
+<style>
+.vue-alert {
+  position: fixed;
+  top: 60px;
+  width: 100%;
+  text-align: center;
+}
+
+.vue-alert .alert-text {
+  display: inline-block;
 }
 </style>
