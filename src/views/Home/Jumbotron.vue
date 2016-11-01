d56156cf540201cb4caaa1446578e6f2ca73deab
MillerRen <ren12345@126.com>
2016-11-01 17:07:55 +0800

    responsive

--- src/views/Home/Jumbotron.vue
+++ src/views/Home/Jumbotron.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="jumbotron">
     <div class="container text-center">
-      <img src="../../assets/awesome-vue.png">
+      <img src="../../assets/awesome-vue.png" class="img-responsive">
       <hello class="text-center"></hello>
     </div>
   </div>
