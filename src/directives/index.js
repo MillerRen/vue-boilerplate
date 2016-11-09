0da91ad2a583ecf056d23ee069d36d6760ec484b
MillerRen <ren12345@126.com>
2016-11-09 17:39:57 +0800

    vue-clickaway directive

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import {directive as Clickaway} from 'vue-clickaway'
 
 // global directive register
+Vue.directive('clickaway', Clickaway)
 Vue.directive('dropdown', require('./dropdown').default)
