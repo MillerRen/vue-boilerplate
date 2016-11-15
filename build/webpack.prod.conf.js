813c77a0385ba6a00c7ca2e7ecca0c66d501a105
MillerRen <ren12345@126.com>
2016-11-15 12:09:53 +0800

    file hash

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -25,8 +25,8 @@ var webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].js?[chunkhash]'),
-    chunkFilename: utils.assetsPath('js/[id].js?[chunkhash]')
+    filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   vue: {
     loaders: utils.cssLoaders({
@@ -46,7 +46,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
     // extract css into its own file
-    new ExtractTextPlugin(utils.assetsPath('css/[name].css?[contenthash]')),
+    new ExtractTextPlugin(utils.assetsPath('css/[name].[contenthash].css')),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsServerDirectory: 'server',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
