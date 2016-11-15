23aa6cce71a6e4754423ab22749bb2d69be53fa3
MillerRen <ren12345@126.com>
2016-11-15 11:47:22 +0800

    build progress

--- build/build.js
+++ build/build.js
@@ -4,7 +4,6 @@ env.NODE_ENV = 'production'
 
 var path = require('path')
 var config = require('../config')
-var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
@@ -13,9 +12,6 @@ console.log(
   '  Built files are meant to be served over an HTTP server.\n' +
   '  Opening index.html over file:// won\'t work.\n'
 )
-
-var spinner = ora('building for production...')
-spinner.start()
 
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 var serverPath = path.join(config.build.assetsRoot, config.build.assetsServerDirectory)
@@ -26,7 +22,6 @@ cp('-R', 'static/*', assetsPath)
 cp('-R', 'server/*', serverPath)
 
 webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
   if (err) throw err
   process.stdout.write(stats.toString({
     colors: true,
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var config = require('../config')
 var utils = require('./utils')
 var webpack = require('webpack')
+var ora = require('ora')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -9,6 +10,9 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
+
+var spinner = ora('building for production...')
+spinner.start()
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -81,6 +85,11 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor']
+    }),
+
+    new webpack.ProgressPlugin(function handler(percentage, msg) {
+      spinner.text = new Number(100 * percentage).toFixed(2) + "%  " + msg
+      if (percentage >= 1) spinner.stop()
     })
   ]
 })
