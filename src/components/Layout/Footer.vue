8a68664dc1615fbfcab9aa17915ee47641cea335
MillerRen <ren12345@126.com>
2016-11-25 15:05:09 +0800

    router view refactor

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<footer class="component-footer footer">
+  <div class="container text-center">
+    <p class="text-muted">Vue-boilerplate@{{version}} powered by Vue</p>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      version: process.env.VERSION
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Layout/Main.vue
@@ -0,0 +1,7 @@
+<template>
+<main class="component-main">
+  <transition name="router" mode="out-in">
+    <router-view class="view router-view"></router-view>
+  </transition>
+</main>
+</template>
