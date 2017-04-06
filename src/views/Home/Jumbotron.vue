3d877b79c2825f7725db12892be224b2e3f92fcc
MillerRen <ren12345@126.com>
2017-04-06 18:38:31 +0800

    remove hello compoennt

--- src/views/Home/Jumbotron.vue
+++ src/views/Home/Jumbotron.vue
@@ -2,7 +2,6 @@
 <div class="jumbotron">
     <div class="container text-center">
       <img src="../../assets/awesome-vue.png" class="img-responsive">
-      <hello class="text-center"></hello>
     </div>
   </div>
 </template>
