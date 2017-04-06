df4661cbe37fd052a402b2b23e8b19f1c0982cad
MillerRen <ren12345@126.com>
2017-04-06 18:40:43 +0800

    revert hello compoennt

--- src/views/Home/Jumbotron.vue
+++ src/views/Home/Jumbotron.vue
@@ -2,6 +2,18 @@
 <div class="jumbotron">
     <div class="container text-center">
       <img src="../../assets/awesome-vue.png" class="img-responsive">
+      <hello class="text-center"></hello>
     </div>
   </div>
 </template>
+
+<script>
+import Hello from '@/components/Hello'
+
+export default {
+  name: 'jumbotron',
+  components: {
+    Hello
+  }
+}
+</script>
