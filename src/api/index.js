50062f57f6608ec70e6ff5afc727cecb43682053
MillerRen <ren12345@126.com>
2017-10-18 03:23:48 +0800

    request progress

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(VueResource)
 
@@ -9,10 +11,16 @@ Vue.http.options.crossOrigin = true
 Vue.http.options.xhr = {withCredentials: true}
 // Vue.http.options.emulateJSON = true
 
+Vue.http.interceptor.before = function (request, next) {
+  NProgress.start()
+  next()
+}
+
 Vue.http.headers.common['Authorization'] = localStorage.getItem('token') ? 'Bearer ' + localStorage.getItem('token') : ''
 
 Vue.http.interceptors.push((request, next) => {
   next((response) => {
+    NProgress.done()
     if (response.status === 401) {
       localStorage.removeItem('token')
     }
@@ -22,7 +30,7 @@ Vue.http.interceptors.push((request, next) => {
 
 // mock data
 if (process.env.NODE_ENV !== 'production') {
-  require('./mock-data')
+  // require('./mock-data')
 }
 
 export const Message = Vue.resource(API_ROOT + '/messages{/id}')
