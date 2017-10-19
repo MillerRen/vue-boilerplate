97ab23b6591065704db13b89d67772cfa5c20f54
MillerRen <ren12345@126.com>
2017-10-19 14:13:11 +0800

    fix public path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,6 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsServerDirectory: 'server',
     assetsPublicPath: '/vue-boilerplate/',
-    publicPath: '/vue-boilerplate/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
