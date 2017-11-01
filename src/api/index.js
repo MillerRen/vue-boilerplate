39dc20ec02159b1e5b746886499d9b74b0959f8b
MillerRen <ren12345@126.com>
2017-11-01 16:39:28 +0800

    axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,40 +1,31 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import axios from 'axios'
 
-Vue.use(VueResource)
+const API_ROOT = '/api'
 
-const API_ROOT = process.env.API_ROOT
+axios.defaults.BASE_URL = API_ROOT
 
-Vue.http.options.crossOrigin = true
-Vue.http.options.xhr = {withCredentials: true}
-// Vue.http.options.emulateJSON = true
-
-Vue.http.interceptor.before = function (request, next) {
+axios.interceptors.request.use(function (request) {
   NProgress.start()
-  next()
-}
-
-Vue.http.headers.common['Authorization'] = localStorage.getItem('token') ? 'Bearer ' + localStorage.getItem('token') : ''
-
-Vue.http.interceptors.push((request, next) => {
-  next((response) => {
-    NProgress.done()
-    if (response.status === 401) {
-      localStorage.removeItem('token')
-    }
-    return response
-  })
+}, function (error) {
+  return Promise.reject(error)
 })
+
+axios.interceptors.response.use(function () {
+  NProgress.done()
+}, function (error) {
+  Vue.toasted.e(error)
+  return Promise.reject(error)
+})
+
+Vue.http = axios
+Vue.prototype.$http = axios
 
 // mock data
 if (process.env.NODE_ENV !== 'production') {
-  require('./mock-data')
+  // require('./mock-data')
 }
 
-export const Message = Vue.resource(API_ROOT + '/messages{/id}')
-
-export const Account = Vue.resource(API_ROOT + '/users{/id}')
-
-export const Auth = Vue.resource('/auth/local')
+export default axios
