b0b9b7574303b25e1213d2443e326a6212683791
MillerRen <ren12345@126.com>
2017-11-01 19:59:04 +0800

    promise polyfill

--- src/api/mock-data.js
+++ src/api/mock-data.js
@@ -1,7 +1,11 @@
-import me from './data/me.json'
-import auth from './data/auth.json'
+// import axios from 'axios'
+// import Mock from 'axios-mock-adapter'
+// import me from './data/me.json'
+// import auth from './data/auth.json'
 
-export default {
-  '/users/me': me,
-  '/auth/local': auth
-}
+// var mock = new Mock(axios)
+
+// export default {
+//   '/users/me': me,
+//   '/auth/local': auth
+// }
